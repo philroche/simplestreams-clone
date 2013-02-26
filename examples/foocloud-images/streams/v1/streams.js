a63fec15a36a12df3ae672e65d8ba8e3a81a1847
Scott Moser <smoser@ubuntu.com>
2013-02-25 21:22:27 -0500

    foocloud-images: yaml2js
    bzr-revno: 117.1.11

---
+++ examples/foocloud-images/streams/v1/streams.js
@@ -0,0 +1,20 @@
+{
+ "format": "stream-collection:1.0", 
+ "streams": [
+  {
+   "path": "streams/v1/i386.js", 
+   "arch": "i386"
+  }, 
+  {
+   "path": "streams/v1/amd64.js", 
+   "arch": "amd64"
+  }
+ ], 
+ "description": "Foovendor Cloud Image streams", 
+ "tags": {
+  "release": "pinky", 
+  "version": 12.04, 
+  "build": "server", 
+  "stream": "released"
+ }
+}
