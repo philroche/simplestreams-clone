e700c82c20a624c706a566a1e182a644a0f3fc75
Scott Moser <smoser@ubuntu.com>
2013-02-25 20:53:31 -0500

    examples/cirros: yaml to json
    bzr-revno: 117.1.3

---
+++ examples/cirros/streams/v1/devel-images-arm.js
@@ -0,0 +1,59 @@
+{
+ "iqn": "iqn.2012-12.net.cirros-cloud:cloud-images:devel:arm", 
+ "format": "stream:1.0", 
+ "item_groups": [
+  {
+   "items": [
+    {
+     "path": "0.3.1~pre4/cirros-0.3.1~pre4-arm-lxc.tar.gz", 
+     "md5": "55b092bde364aad125a1db57c932f1d0", 
+     "name": "cirros-0.3.1~pre4-arm-lxc.tar.gz", 
+     "size": 3466163
+    }, 
+    {
+     "path": "0.3.1~pre4/cirros-0.3.1~pre4-arm-rootfs.img.gz", 
+     "md5": "986c9cabd412f12cb5027b7d7eb4ec03", 
+     "name": "cirros-0.3.1~pre4-arm-rootfs.img.gz", 
+     "size": 10949810
+    }, 
+    {
+     "path": "0.3.1~pre4/cirros-0.3.1~pre4-arm-uec.tar.gz", 
+     "md5": "797e2d488c799eab0a8eb09a9c1ff4a3", 
+     "name": "cirros-0.3.1~pre4-arm-uec.tar.gz", 
+     "size": 7314153
+    }
+   ], 
+   "serial": 20130111, 
+   "version": "0.3.1~pre4"
+  }, 
+  {
+   "items": [
+    {
+     "path": "0.3.1~pre3/cirros-0.3.1~pre3-arm-lxc.tar.gz", 
+     "size": 3456925, 
+     "name": "cirros-0.3.1~pre3-arm-lxc.tar.gz", 
+     "md5": "fbc83952ca51071c1aea8b0d82c51d1b"
+    }, 
+    {
+     "path": "0.3.1~pre3/cirros-0.3.1~pre3-arm-rootfs.img.gz", 
+     "size": 10922886, 
+     "name": "cirros-0.3.1~pre3-arm-rootfs.img.gz", 
+     "md5": "c98688744bdf66b56b19405acfc48966"
+    }, 
+    {
+     "path": "0.3.1~pre3/cirros-0.3.1~pre3-arm-uec.tar.gz", 
+     "size": 7302383, 
+     "name": "cirros-0.3.1~pre3-arm-uec.tar.gz", 
+     "md5": "d1703407ad1483e2bbf68d4d78987581"
+    }
+   ], 
+   "serial": 20101020, 
+   "version": "0.3.1~pre3"
+  }
+ ], 
+ "description": "Cirros Cloud Released images for arm", 
+ "tags": {
+  "arch": "arm", 
+  "stream": "devel"
+ }
+}
