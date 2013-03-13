4af841e5f664301b3512184bcfebf373c8b40697
Scott Moser <smoser@ubuntu.com>
2013-03-12 21:00:28 -0400

    change default size to be None, not -1
    bzr-revno: 132

--- simplestreams/objectstores.py
+++ simplestreams/objectstores.py
@@ -304,7 +304,7 @@ class Reader(object):
         self.reader = reader
         self.url = url
 
-    def read(self, size=-1):
+    def read(self, size=None):
         return self.reader.read(size)
 
     def __enter__(self):
