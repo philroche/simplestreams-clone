89cdea3c3891c1027d13fe6c9adc047687f30c46
Scott Moser <smoser@ubuntu.com>
2013-03-18 14:54:09 -0400

    re-working of mirrors to allow for hopefully easier re-use of base class.
    bzr-revno: 134.2.10

--- simplestreams/mirrors.py
+++ simplestreams/mirrors.py
@@ -15,7 +15,8 @@ class MirrorWriter(object):
     def load_products(self, path=None):
         raise NotImplementedError()
 
-    def sync_products(self, reader, path=None, products=None, content=None):
+    def sync_products(self, reader, path=None, products=None,
+                      content=None):
         # reader:   a Reader for opening files referenced in products
         # path:     the path of where to store this.
         #           if path is None, do not store the products file itself
@@ -30,7 +31,7 @@ class MirrorWriter(object):
         #  * if products is None, it will be loaded from content
         raise NotImplementedError()
 
-    def sync_index(self, reader, path=None, index=None, content=None):
+    def sync_index(self, reader, path=None, index=None,content=None):
         # reader:   a Reader for opening files referenced in index or products
         #           files
         # path:     the path of where to store this.
@@ -58,24 +59,36 @@ class MirrorWriter(object):
         else:
             raise TypeError("Unknown format '%s' in '%s'" % (fmt, path))
 
-    def filter_index_entry(self, entry):
-        _x = entry
+    def filter_index_entry(self, content_id, content, tree, pedigree):
         return True
 
-    def filter_product(self, product_data):
-        (_name, _data) = product_data
+    def filter_product(self, product_id, product, tree, pedigree):
         return True
 
-    def filter_version(self, version_data, product_data):
-        (_pname, _pdata) = product_data
-        (_vname, _vdata) = version_data
+    def filter_version(self, version_id, version, tree, pedigree):
         return True
 
-    def filter_item(self, item, product_data, version_data):
-        (_pname, _pdata) = product_data
-        (_vname, _vdata) = version_data
-        _x = item
+    def filter_item(self, item_id, item, tree, pedigree):
         return True
+
+    def insert_index(self, path, index, content):
+        pass
+
+    def insert_index_entry(self, contentsource, content_id, content, tree,
+                           pedigree):
+        pass
+
+    def insert_products(self, path, products, content):
+        pass
+
+    def insert_product(self, product_id, product, tree, pedigree):
+        pass
+
+    def insert_version(self, version_id, version, tree, pedigree):
+        pass
+
+    def insert_item(self, contentsource, item_id, item, tree, pedigree):
+        pass
 
 
 class UrlMirrorReader(MirrorReader):
@@ -95,10 +108,73 @@ class ObjectStoreMirrorReader(MirrorReader):
         return self.objectstore.reader(path)
 
 
-class ObjectStoreMirrorWriter(MirrorWriter):
-    def __init__(self, config, objectstore):
-        self.store = objectstore
+class BasicMirrorWriter(MirrorWriter):
+    def __init__(self, config):
+        super(BasicMirrorWriter, self).__init__()
         self.config = config
+
+    def sync_index(self, reader, path=None, index=None, content=None):
+        (index, content) = _get_data_content(path, index, content, reader)
+
+        util.expand_tree(index)
+
+        itree = index.get('index')
+        for content_id, index_entry in itree.iteritems():
+            if not self.filter_index_entry(content_id, index_entry, index,
+                                           (content_id)):
+                continue
+            epath = index_entry.get('path', None)
+            epath_cs = None
+            if epath:
+                if index_entry.get('format') in ("index:1.0", "products:1.0"):
+                    self.sync(reader, path=epath)
+                epath_cs = reader(epath)
+
+            self.insert_index_entry(epath_cs, content_id, index_entry, index,
+                                    (content_id,))
+
+        self.insert_index(path, index, content)
+
+    def sync_products(self, reader, path=None, products=None, content=None):
+        (products, content) = _get_data_content(path, products, content,
+                                                reader)
+
+        util.expand_tree(products)
+
+        ptree = products.get('products', {})
+        for prodname, product in ptree.iteritems():
+            if not self.filter_product(prodname, product, products,
+                                       (prodname,)):
+                continue
+
+            for vername, version in product.get('versions', {}).iteritems():
+                if not self.filter_version(vername, version, products,
+                                           (prodname, vername)):
+                    continue
+
+                for itemname, item in version.get('items', {}).iteritems():
+                    pgree = (prodname, vername, itemname)
+                    if not self.filter_item(itemname, item, products, pgree):
+                        continue
+
+                    ipath = item.get('path', None)
+                    ipath_cs = None
+                    if ipath:
+                        ipath_cs = reader(ipath)
+                    self.insert_item(ipath_cs, itemname, item, products, pgree)
+
+                self.insert_version(vername, version, products,
+                                    (prodname, vername))
+
+            self.insert_product(prodname, product, products, (prodname,))
+
+        self.insert_products(path, products, content)
+
+
+class ObjectStoreMirrorWriter(BasicMirrorWriter):
+    def __init__(self, config, objectstore):
+        super(ObjectStoreMirrorWriter, self).__init__(config=config)
+        self.store = objectstore
 
     def load_products(self, path=None):
         if path:
@@ -109,63 +185,32 @@ class ObjectStoreMirrorWriter(MirrorWriter):
     def reader(self, path):
         return self.store.reader(path)
 
-    def sync_index(self, reader, path=None, index=None, content=None):
-        (index, content) = _get_data_content(path, index, content, reader)
+    def insert_item(self, cs, itemname, item, products, pedigree):
+        if 'path' not in item:
+            return
+        print "inserting %s" % item['path']
+        self.store.insert(item['path'], cs,
+                          checksums=util.item_checksums(item), mutable=False)
 
-        util.expand_tree(index)
+    def insert_index_entry(self, cs, content_id, content, tree, pedigree):
+        epath = content.get('path', None)
+        if not epath:
+            return
+        self.store.insert(epath, cs, checksums=util.item_checksums(content))
 
-        itree = index.get('index')
-        for entry in itree:
-            if not self.filter_index_entry(entry):
-                continue
-            if 'path' in entry:
-                epath = entry['path']
-                if entry.get('format') in ("index:1.0", "products:1.0"):
-                    self.sync(reader, path=epath)
-                else:
-                    self.store.insert(epath, reader(epath),
-                                      checksums=util.item_checksums(entry))
-        if path:
-            if not content:
-                content = util.dump_data(index)
-            self.store.insert_content(path, content)
+    def insert_products(self, path, products, content):
+        if not path:
+            return
+        if not content:
+            content = util.dump_data(products)
+        self.store.insert_content(path, content)
 
-    def sync_products(self, reader, path=None, products=None, content=None):
-        (products, content) = _get_data_content(path, products, content,
-                                                reader)
-
-        util.expand_tree(products)
-
-        ptree = products.get('products', {})
-        for prodname, product in ptree.iteritems():
-            if not self.filter_product(product_data=pdata):
-                continue
-            #print " insert product %s" % pdata[0]
-
-            product = pdata[1]
-            for verdata in product.get('versions', {}).iteritems():
-                if not self.filter_version(version_data=verdata,
-                                           product_data=pdata):
-                    continue
-
-                #print "  insert version %s" % verdata[0]
-
-                version = verdata[1]
-                for item in version.get('items', []):
-                    if not self.filter_item(item=item, product_data=pdata,
-                                            version_data=verdata):
-                        continue
-
-                    print "   insert item %s" % item
-                    if 'path' in item:
-                        self.store.insert(item['path'], reader(item['path']),
-                                          checksums=util.item_checksums(item),
-                                          mutable=False)
-
-        if path:
-            if not content:
-                content = util.dump_data(products)
-            self.store.insert_content(path, content)
+    def insert_index(self, path, index, content):
+        if not path:
+            return
+        if not content:
+            content = util.dump_data(index)
+        self.store.insert_content(path, content)
 
 
 def _get_data_content(path, data, content, reader):
