8affba095966b8a60894a65d8df980c701569de6
Scott Moser <smoser@ubuntu.com>
2013-03-25 21:06:37 -0400

    add setup.py
    bzr-revno: 176

---
+++ setup.py
@@ -0,0 +1,28 @@
+VERSION = '0.1.0'
+
+from distutils.core import setup
+from glob import glob
+import os
+
+
+def is_f(p):
+    return os.path.isfile(p)
+
+
+setup(
+    name="python-simplestreams",
+    description='Library and tools for using Simple Streams data',
+    version=VERSION,
+    author='Scott Moser',
+    author_email='scott.moser@canonical.com',
+    license="AGPL",
+    url='http://launchpad.net/simplestreams/',
+    packages=['simplestreams', 'simplestreams.mirrors',
+              'simplestreams.objectstores'],
+    scripts=['bin/sstream-sync'],
+    data_files=[
+        ('/usr/lib/simplestreams', glob('tools/hook-*')),
+        ('/usr/share/doc/simplestreams',
+         [f for f in glob('doc/*') if is_f(f)]),
+    ]
+)
