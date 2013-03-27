55b46bc388cd3f559dda46f7a3f99b5ddce85cf1
Scott Moser <smoser@ubuntu.com>
2013-03-27 15:35:12 -0400

    foocloud: fix names and add 'ftype'
    bzr-revno: 194

--- examples/foocloud/streams/v1/com.example.foovendor:released:download.js
+++ examples/foocloud/streams/v1/com.example.foovendor:released:download.js
@@ -11,19 +11,22 @@
     "20121026.1": {
      "items": {
       "tar.gz": {
-       "name": "foovendor-pinky-12.04-amd64-server-20121026.1.tar.gz", 
+       "name": "foovendor-pinky-6.1-amd64-server-20121026.1.tar.gz", 
        "path": "files/release-20121026.1/foovendor-6.1-server-cloudimg-amd64.tar.gz", 
-       "md5": "187ea3b68f9080d4c447b910c8d0838e"
+       "md5": "187ea3b68f9080d4c447b910c8d0838e",
+       "ftype": "tar.gz",
       }, 
       "disk1.img": {
-       "name": "foovendor-pinky-12.04-amd64-server-20121026.1-disk1.img", 
+       "name": "foovendor-pinky-6.1-amd64-server-20121026.1-disk1.img", 
        "path": "files/release-20121026.1/foovendor-6.1-server-cloudimg-amd64-disk1.img", 
-       "md5": "da499b246265a34db8576983371c6c2a"
+       "md5": "da499b246265a34db8576983371c6c2a",
+       "ftype": "disk1.img",
       }, 
       "root.tar.gz": {
-       "name": "foovendor-pinky-12.04-amd64-server-20121026.1-root.tar.gz", 
+       "name": "foovendor-pinky-6.1-amd64-server-20121026.1-root.tar.gz", 
        "path": "files/release-20121026.1/foovendor-6.1-server-cloudimg-amd64-root.tar.gz", 
-       "md5": "da939f41059a1b1eca16c0c5856f47cd"
+       "md5": "da939f41059a1b1eca16c0c5856f47cd",
+       "ftype": "root.tar.gz",
       }
      }, 
      "label": "release"
@@ -31,19 +34,22 @@
     "20120328": {
      "items": {
       "tar.gz": {
-       "name": "foovendor-pinky-12.04-beta2-amd64-server-20120328.tar.gz", 
+       "name": "foovendor-pinky-6.1-beta2-amd64-server-20120328.tar.gz", 
        "path": "files/beta-2/foovendor-6.1-beta2-server-cloudimg-amd64.tar.gz", 
-       "md5": "c245123c1a7c16dd43962b71c604c5ee"
+       "md5": "c245123c1a7c16dd43962b71c604c5ee",
+       "ftype": "tar.gz",
       }, 
       "disk1.img": {
-       "name": "foovendor-pinky-12.04-beta2-amd64-server-20120328-disk1.img", 
+       "name": "foovendor-pinky-6.1-beta2-amd64-server-20120328-disk1.img", 
        "path": "files/beta-2/foovendor-6.1-beta2-server-cloudimg-amd64-disk1.img", 
-       "md5": "34cec541a18352783e736ba280a12201"
+       "md5": "34cec541a18352783e736ba280a12201",
+       "ftype": "disk1.img",
       }, 
       "root.tar.gz": {
-       "name": "foovendor-pinky-12.04-beta2-amd64-server-20120328-root.tar.gz", 
+       "name": "foovendor-pinky-6.1-beta2-amd64-server-20120328-root.tar.gz", 
        "path": "files/beta-2/foovendor-6.1-beta2-server-cloudimg-amd64-root.tar.gz", 
-       "md5": "55686ef088f7baf0ebea9349055daa85"
+       "md5": "55686ef088f7baf0ebea9349055daa85",
+       "ftype": "root.tar.gz",
       }
      }, 
      "label": "beta2"
@@ -51,19 +57,22 @@
     "20121001": {
      "items": {
       "tar.gz": {
-       "name": "foovendor-pinky-12.04-amd64-server-20121001.tar.gz", 
+       "name": "foovendor-pinky-6.1-amd64-server-20121001.tar.gz", 
        "path": "files/release-20121001/foovendor-6.1-server-cloudimg-amd64.tar.gz", 
-       "md5": "187ea3b68f9080d4c447b910c8d0838e"
+       "md5": "187ea3b68f9080d4c447b910c8d0838e",
+       "ftype": "tar.gz",
       }, 
       "disk1.img": {
-       "name": "foovendor-pinky-12.04-amd64-server-20121001-disk1.img", 
+       "name": "foovendor-pinky-6.1-amd64-server-20121001-disk1.img", 
        "path": "files/release-20121001/foovendor-6.1-server-cloudimg-amd64-disk1.img", 
-       "md5": "da499b246265a34db8576983371c6c2a"
+       "md5": "da499b246265a34db8576983371c6c2a",
+       "ftype": "disk1.img",
       }, 
       "root.tar.gz": {
-       "name": "foovendor-pinky-12.04-amd64-server-20121001-root.tar.gz", 
+       "name": "foovendor-pinky-6.1-amd64-server-20121001-root.tar.gz", 
        "path": "files/release-20121001/foovendor-6.1-server-cloudimg-amd64-root.tar.gz", 
-       "md5": "da939f41059a1b1eca16c0c5856f47cd"
+       "md5": "da939f41059a1b1eca16c0c5856f47cd",
+       "ftype": "root.tar.gz",
       }
      }, 
      "label": "release"
@@ -80,19 +89,22 @@
     "20121026.1": {
      "items": {
       "tar.gz": {
-       "name": "foovendor-pinky-12.04-i386-server-20121026.1.tar.gz", 
+       "name": "foovendor-pinky-6.1-i386-server-20121026.1.tar.gz", 
        "path": "files/release-20121026.1/foovendor-6.1-server-cloudimg-i386.tar.gz", 
-       "md5": "1534e487730c8162131dde430aa5fa5a"
+       "md5": "1534e487730c8162131dde430aa5fa5a",
+       "ftype": "tar.gz",
       }, 
       "disk1.img": {
-       "name": "foovendor-pinky-12.04-i386-server-20121026.1-disk1.img", 
+       "name": "foovendor-pinky-6.1-i386-server-20121026.1-disk1.img", 
        "path": "files/release-20121026.1/foovendor-6.1-server-cloudimg-i386-disk1.img", 
-       "md5": "d8fa9a536e8cf1bebcee1e26875060bb"
+       "md5": "d8fa9a536e8cf1bebcee1e26875060bb",
+       "ftype": "disk1.img",
       }, 
       "root.tar.gz": {
-       "name": "foovendor-pinky-12.04-i386-server-20121026.1-root.tar.gz", 
+       "name": "foovendor-pinky-6.1-i386-server-20121026.1-root.tar.gz", 
        "path": "files/release-20121026.1/foovendor-6.1-server-cloudimg-i386-root.tar.gz", 
-       "md5": "de2b46e0fe2ce8c6eda2b4cd809505a9"
+       "md5": "de2b46e0fe2ce8c6eda2b4cd809505a9",
+       "ftype": "root.tar.gz",
       }
      }, 
      "label": "release"
@@ -100,19 +112,22 @@
     "20120328": {
      "items": {
       "tar.gz": {
-       "name": "foovendor-pinky-12.04-beta2-i386-server-20120328.tar.gz", 
+       "name": "foovendor-pinky-6.1-beta2-i386-server-20120328.tar.gz", 
        "path": "files/beta-2/foovendor-6.1-beta2-server-cloudimg-i386.tar.gz", 
-       "md5": "2cd18b60f892af68c9d49c64ce1638e4"
+       "md5": "2cd18b60f892af68c9d49c64ce1638e4",
+       "ftype": "tar.gz",
       }, 
       "disk1.img": {
-       "name": "foovendor-pinky-12.04-beta2-i386-server-20120328-disk1.img", 
+       "name": "foovendor-pinky-6.1-beta2-i386-server-20120328-disk1.img", 
        "path": "files/beta-2/foovendor-6.1-beta2-server-cloudimg-i386-disk1.img", 
-       "md5": "e80df7995beb31571e104947e4d7b001"
+       "md5": "e80df7995beb31571e104947e4d7b001",
+       "ftype": "disk1.img",
       }, 
       "root.tar.gz": {
-       "name": "foovendor-pinky-12.04-beta2-i386-server-20120328-root.tar.gz", 
+       "name": "foovendor-pinky-6.1-beta2-i386-server-20120328-root.tar.gz", 
        "path": "files/beta-2/foovendor-6.1-beta2-server-cloudimg-i386-root.tar.gz", 
-       "md5": "5d86b3e75e56e10e1019fe1153fe488f"
+       "md5": "5d86b3e75e56e10e1019fe1153fe488f",
+       "ftype": "root.tar.gz",
       }
      }, 
      "label": "beta2"
@@ -120,19 +135,22 @@
     "20121001": {
      "items": {
       "tar.gz": {
-       "name": "foovendor-pinky-12.04-i386-server-20121001.tar.gz", 
-       "path": "files/release-20121001/foovendor-6.1-server-cloudimg-i386.tar.gz", 
+       "name": "foovendor-pinky-6.1-i386-server-20121001.tar.gz", 
+       "path": "files/release-20121001/foovendor-6.1-server-cloudimg-i386.tar.gz", ,
+       "ftype": "tems",
        "md5": "1534e487730c8162131dde430aa5fa5a"
       }, 
       "disk1.img": {
-       "name": "foovendor-pinky-12.04-i386-server-20121001-disk1.img", 
+       "name": "foovendor-pinky-6.1-i386-server-20121001-disk1.img", 
        "path": "files/release-20121001/foovendor-6.1-server-cloudimg-i386-disk1.img", 
-       "md5": "d8fa9a536e8cf1bebcee1e26875060bb"
+       "md5": "d8fa9a536e8cf1bebcee1e26875060bb",
+       "ftype": "disk1.img",
       }, 
       "root.tar.gz": {
-       "name": "foovendor-pinky-12.04-i386-server-20121001-root.tar.gz", 
+       "name": "foovendor-pinky-6.1-i386-server-20121001-root.tar.gz", 
        "path": "files/release-20121001/foovendor-6.1-server-cloudimg-i386-root.tar.gz", 
-       "md5": "de2b46e0fe2ce8c6eda2b4cd809505a9"
+       "md5": "de2b46e0fe2ce8c6eda2b4cd809505a9",
+       "ftype": "root.tar.gz",
       }
      }, 
      "label": "release"
