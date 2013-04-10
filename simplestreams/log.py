a32dce83dc87e50627b2ab2afe2337fcfe03179b
Scott Moser <smoser@ubuntu.com>
2013-04-10 18:48:15 +0000

    improve logging.
    Now simplestreams.log.basicConfig does not just call
    logging.basicConfig.  The value in that is that now,
      log.basicConfig()
    will get a basicConfig() set up for 'sstreams' but not
    anything else.
    
    This was annoying because if we used the logging.basicConfig()
    then we'd see other libraries log messages go by.
    
    bzr-revno: 215.1.1

--- simplestreams/log.py
+++ simplestreams/log.py
@@ -1,14 +1,44 @@
 import logging
 from logging import (DEBUG, ERROR, FATAL, INFO, NOTSET, WARN, WARNING)
 
+
+class NullHandler(logging.Handler):
+    def emit(self, record):
+        pass
+
+
 def basicConfig(**kwargs):
-    log = logging.getLogger()
-    for h in log.handlers:
-        log.removeHandler(h)
-    logging.basicConfig(**kwargs)
+    # basically like logging.basicConfig but only output for our logger
+    if kwargs.get('filename'):
+        handler = logging.FileHandler(filename=kwargs['filename'],
+                                      mode=kwargs.get('filemode', 'a'))
+    elif kwargs.get('stream'):
+        handler = logging.StreamHandler(stream=kwargs['stream'])
+    else:
+        handler = NullHandler()
+
+    level = kwargs.get('level', NOTSET)
+
+    handler.setFormatter(logging.Formatter(fmt=kwargs.get('format'),
+                                           datefmt=kwargs.get('datefmt')))
+    handler.setLevel(level)
+
+    logging.getLogger().setLevel(level)
+
+    logger = _getLogger()
+    for h in list(logger.handlers):
+        logger.removeHandler(h)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+
+    print "HI"
 
 
-basicConfig()
-LOG = logging.getLogger(name='sstreams')
+def _getLogger(name='sstreams'):
+    return logging.getLogger(name)
+
+
+logging.basicConfig()
+LOG = _getLogger()
 
 # vi: ts=4 expandtab syntax=python
