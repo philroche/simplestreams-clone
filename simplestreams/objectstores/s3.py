af7aa0cea438195d121f7642c0963ede77f3d7a3
Scott Moser <smoser@ubuntu.com>
2013-08-22 08:59:37 -0400

    add license / copyright headers
    bzr-revno: 277

--- simplestreams/objectstores/s3.py
+++ simplestreams/objectstores/s3.py
@@ -1,3 +1,20 @@
+#   Copyright (C) 2013 Canonical Ltd.
+#
+#   Author: Scott Moser <scott.moser@canonical.com>
+#
+#   Simplestreams is free software: you can redistribute it and/or modify it
+#   under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or (at your
+#   option) any later version.
+#
+#   Simplestreams is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+#   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+#   License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with Simplestreams.  If not, see <http://www.gnu.org/licenses/>.
+
 import boto.exception
 import boto.s3
 import boto.s3.connection
