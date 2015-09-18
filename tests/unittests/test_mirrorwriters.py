3ab765109c6ee417b4c23eabf92656d47c833f44
Scott Moser <smoser@ubuntu.com>
2015-09-18 09:41:23 -0400

    fix incorrect size in example and adjust test
    also, put these in a UnitTest class so we can use assertEqual.
    
    bzr-revno: 398.1.7

--- tests/unittests/test_mirrorwriters.py
+++ tests/unittests/test_mirrorwriters.py
@@ -3,25 +3,26 @@ from simplestreams.filters import get_filters
 from simplestreams.mirrors import DryRunMirrorWriter, ObjectFilterMirror
 from simplestreams.objectstores import MemoryObjectStore
 
-
-def test_DryRunMirrorWriter_foocloud_no_filters():
-    src = get_mirror_reader("foocloud")
-    config = {}
-    objectstore = MemoryObjectStore(None)
-    target = DryRunMirrorWriter(config, objectstore)
-    target.sync(src, "streams/v1/index.json")
-    assert target.size == 1311
+from unittest import TestCase
 
 
-def test_ObjectFilterMirror_does_item_filter():
-    src = get_mirror_reader("foocloud")
-    filter_list = get_filters(['ftype!=disk1.img'])
-    config = {'filters': filter_list}
-    objectstore = MemoryObjectStore(None)
-    target = ObjectFilterMirror(config, objectstore)
-    target.sync(src, "streams/v1/index.json")
+class TestMirrorWriters(TestCase):
+    def test_DryRunMirrorWriter_foocloud_no_filters(self):
+        src = get_mirror_reader("foocloud")
+        config = {}
+        objectstore = MemoryObjectStore(None)
+        target = DryRunMirrorWriter(config, objectstore)
+        target.sync(src, "streams/v1/index.json")
+        self.assertEqual(1277, target.size)
 
-    unexpected = [f for f in objectstore.data if 'disk' in f]
+    def test_ObjectFilterMirror_does_item_filter(self):
+        src = get_mirror_reader("foocloud")
+        filter_list = get_filters(['ftype!=disk1.img'])
+        config = {'filters': filter_list}
+        objectstore = MemoryObjectStore(None)
+        target = ObjectFilterMirror(config, objectstore)
+        target.sync(src, "streams/v1/index.json")
 
-    assert len(unexpected) == 0
-    assert len(objectstore.data) != 0
+        unexpected = [f for f in objectstore.data if 'disk' in f]
+        assert len(unexpected) == 0
+        assert len(objectstore.data) != 0
