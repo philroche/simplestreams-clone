b75d0bdc5cf705cefe14d310c99458f3ea1795ea
David Ames <david.ames@canonical.com>
2017-09-11 13:33:33 -0700

    Use dict.get
    bzr-revno: 449.2.7

--- simplestreams/mirrors/glance.py
+++ simplestreams/mirrors/glance.py
@@ -272,7 +272,7 @@ class GlanceMirror(mirrors.BasicMirrorWriter):
                 name_old, name_new = carry_over_property
             else:
                 name_old = name_new = carry_over_property
-            properties[name_new] = image_metadata[name_old]
+            properties[name_new] = image_metadata.get(name_old)
 
         if 'arch' in image_metadata:
             properties['architecture'] = canonicalize_arch(
